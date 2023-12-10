0c0dc4a2086693f8dee11a8c3c27cb4f16a3c27f
Kabir Advani <kabiradvani@0587462192.wireless.umich.net>
2023-12-09 21:27:33 -0500

    wrote get_api_info()

--- NBAData.py
+++ NBAData.py
@@ -2,5 +2,15 @@
 
 import requests
 import json
+import sqlite3
 import unittest
 import os
+
+def get_api_info(url, params=None):
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        return response.json()
+    except:
+        print("Exception!")
+        return None
