fd19e711483e652cf127f242cf8a2a8e90aaee1d
Kabir Advani <kabiradvani@Lalo-Salamanca.local>
2023-12-10 15:41:54 -0500

    wrote set_up_team_table()

--- NBAData.py
+++ NBAData.py
@@ -22,9 +22,6 @@ def get_api_info(url, api_key, teamNum):
     except requests.exceptions.RequestException as e:
         return f"Request failed: {e}"
 
-def get_api2(url, api_key):
-    pass
-
 def count_team_wins_losses(data, team_id):
     home_wins = 0
     home_losses = 0
@@ -48,6 +45,46 @@ def count_team_wins_losses(data, team_id):
     finalDict = {'Home Wins':home_wins, 'Home Losses':home_losses, 'Away Wins':away_wins, 'Away Losses':away_losses}
     return finalDict
 
+def set_up_database(db):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path + "/" + db)
+    cur = conn.cursor()
+    return cur, conn
+
+def set_up_team_table(cur, conn):
+    url = "https://api-nba-v1.p.rapidapi.com/teams"
+    headers = {
+	    "X-RapidAPI-Key": "efc6e04bccmsh098566e98580557p1c5882jsn2d733f042025",
+	    "X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers)
+    teamNameData = response.json()
+    teamList = []
+    for team in teamNameData['response']:
+        if team['name'] in ["Atlanta Hawks", "Boston Celtics", "Brooklyn Nets", "Charlotte Hornets", 
+                            "Chicago Bulls", "Cleveland Cavaliers", "Dallas Mavericks", "Denver Nuggets",
+                            "Detroit Pistons", "Golden State Warriors", "Houston Rockets", "Indiana Pacers",
+                            "LA Clippers", "Los Angeles Lakers", "Memphis Grizzlies",
+                            "Miami Heat", "Milwaukee Bucks", "Minnesota Timberwolves", "New Orleans Pelicans",
+                            "New York Knicks", "Oklahoma City Thunder", "Orlando Magic", "Philadelphia 76ers",
+                            "Phoenix Suns", "Portland Trail Blazers", "Sacramento Kings", "San Antonio Spurs",
+                            "Toronto Raptors", "Utah Jazz", "Washington Wizards"]:
+            teamList.append((team['id'], team['name']))
+    print(teamList)
+    print(len(teamList))
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS teams (
+                        team_id INT PRIMARY KEY,
+                        team_name TEXT NOT NULL
+                      )''')
+
+    
+    cur.executemany('INSERT OR IGNORE INTO teams (team_id, team_name) VALUES (?, ?)', teamList)
+
+    conn.commit()
+    conn.close()
+
+
 def main():
     api_key = "efc6e04bccmsh098566e98580557p1c5882jsn2d733f042025"
     url = "https://api-nba-v1.p.rapidapi.com/games/" 
@@ -55,6 +92,8 @@ def main():
     #print(data)
     record = count_team_wins_losses(data, 1)
     print(record)
+    cur, conn = set_up_database("NBAData.db")
+    set_up_team_table(cur, conn)
     
     
 
