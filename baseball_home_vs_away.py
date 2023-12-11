b5cf912cebf25470c509b96b15e3921d9d0a7bfb
Jacob Ellenbogen <jellenbo@umich.edu>
2023-12-11 13:20:45 -0500

    first function

--- baseball_home_vs_away.py
+++ baseball_home_vs_away.py
@@ -1,9 +1,33 @@
 import re
 import statsapi
+import requests
+import _sqlite3
+import matplotlib as plt
+import seaborn as sb
+import json
+
+def buildTeamList():
+    teamjson = statsapi.get('teams', {'season':'2023'})['teams'] #{'leagueIds':'Major League Baseball'}
+    teamLst = []
+    for team in teamjson:
+        if team['sport']['name'] == "Major League Baseball":
+            teamLst.append(team['shortName'])
+            print(team['shortName'])
+    print(len(teamLst))
+    #teamDict = teamjson
+    #mlbDict = [team for team in teamDict if (team['teams'][0]['sport']['name']) == "Major League Baseball"]
+    #team = statsapi.lookup_team('det')
+    #print(team[0]['id'])
+
+def buildRosterDatabases():
+    pass
+
+def processBoxScore():
+    pass
 
 
 
 def main():
-    print('Hello World')
+    buildTeamList()
 
 main()
