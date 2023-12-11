6c6304aa4a67b90700515d144785a5a385fb1304
Jacob Ellenbogen <jellenbo@umich.edu>
2023-12-11 16:59:59 -0500

    data dealt with

--- baseball_home_vs_away.py
+++ baseball_home_vs_away.py
@@ -66,9 +66,9 @@ def createDatabase(statList, dbName, tableName):
 def processBoxScore(gamePk):
     game = statsapi.game_scoring_play_data(gamePk)
     homeTeam = game['home']['name']
-    homeId = game['home']['team_id']
+    homeId = game['home']['id']
     awayTeam = game['away']['name']
-    awayId = game['away']['team_id']
+    awayId = game['away']['id']
     homeScore = game['plays'][-1]['result']['homeScore']
     awayScore = game['plays'][-1]['result']['awayScore']
     homeTeamWin = homeScore > awayScore
@@ -76,21 +76,50 @@ def processBoxScore(gamePk):
     return (homeTeam, homeId, homeTeamWin, homeScore, awayScore), (awayTeam, awayId, awayTeamWin, awayScore, homeScore)
 
 def createDataDict(season):
-    resultsDict = {}
+    homeResultsDict = {}
+    awayResultsDict = {}
     season_startdate, season_enddate = statsapi.get('season', {'seasonId': season, 'sportId': 1})['seasons'][0]['regularSeasonStartDate'], statsapi.get('season', {'seasonId': season, 'sportId': 1})['seasons'][0]['regularSeasonEndDate'], 
     firstgamepk = statsapi.get('schedule', {'sportId': 1, 'date': season_startdate})['dates'][0]['games'][0]['gamePk']
     lastgamepk = statsapi.get('schedule', {'sportId': 1, 'date': season_enddate})['dates'][0]['games'][-1]['gamePk']
-    for gamepk in range(firstgamepk, lastgamepk + 1): 
+    gamedate = statsapi.get('schedule', {'sportId': 1, 'startDate': season_startdate, 'endDate': season_enddate})['dates']
+    gamepk_lst = []
+    for date in gamedate:
+        for i in range(0, len(date['games']), 1):
+            gamepk_lst.append(date['games'][i]['gamePk'])
+    for gamepk in gamepk_lst: 
         homeData, awayData = processBoxScore(gamepk)
-        if homeData[0] not in resultsDict:
-            homeWin = 0
-            homeLoss = 0
-            if homeData[2] == True:
-                homeWin = 1
-            else:
-                homeLoss = 1
+        homeWin = 0
+        homeLoss = 0
+        awayWin = 0
+        awayLoss = 0
+        if homeData[2] == True:
+            homeWin = 1
+            awayLoss = 1
+        else:
+            homeLoss = 1
+            awayWin = 1
+        if homeData[0] not in homeResultsDict:
             innerDict = {'TeamID': homeData[1], 'HOMEWins' : homeWin, 'HOMELosses': homeLoss, "HOMERScored": homeData[3], 'HOMERAllowed': homeData[4]}
-            resultsDict[homeData[0]] = innerDict
+            homeResultsDict[homeData[0]] = innerDict
+        else:
+            homeResultsDict[homeData[0]]['HOMEWins'] += homeWin
+            homeResultsDict[homeData[0]]['HOMELosses'] += homeLoss
+            homeResultsDict[homeData[0]]['HOMERScored'] += homeData[3]
+            homeResultsDict[homeData[0]]['HOMERAllowed'] += homeData[4]
+
+        if awayData[0] not in awayResultsDict:
+            innerDict = {'TeamID': awayData[1], 'AWAYWins' : awayWin, 'AWAYLosses': awayLoss, "AWAYRScored": awayData[3], 'AWAYRAllowed': awayData[4]}
+            awayResultsDict[awayData[0]] = innerDict
+        else:
+            awayResultsDict[awayData[0]]['AWAYWins'] += awayWin
+            awayResultsDict[awayData[0]]['AWAYLosses'] += awayLoss
+            awayResultsDict[awayData[0]]['AWAYRScored'] += awayData[3]
+            awayResultsDict[awayData[0]]['AWAYRAllowed'] += awayData[4]
+    return homeResultsDict, awayResultsDict
+
+
+
+
             
 
 
@@ -112,7 +141,7 @@ def main():
     #createDatabase(statLst, "Baseball Data.db", "HomeData")
     #createDatabase(statLst, "Baseball Data.db", "AwayData")
     #populateTable()
-    #processBoxScore(gamePk)
+    #processBoxScore(565997)
     createDataDict('2023')
 
 
