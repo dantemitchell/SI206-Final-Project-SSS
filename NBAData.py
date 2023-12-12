4bd44298539b26e83b2e09e85b8fb58d16a4ff56
Kabir Advani <kabiradvani@Lalo-Salamanca.local>
2023-12-12 12:36:14 -0500

    done

--- NBAData.py
+++ NBAData.py
@@ -325,15 +325,13 @@ def calculate_averages_and_point_differential_and_write_to_file(db_path):
     return file_path
 
 def main():
-    record_list = create_record_list_2019()
+    record_list = create_record_list_2022()
     cur, conn = set_up_database("NBAData2.db")
     set_up_team_table(cur, conn)
     set_up_record_table(record_list, cur, conn)
 
     updated_output_file_path = calculate_averages_and_point_differential_and_write_to_file('NBAData2.db')
     updated_output_file_path
-
-
 
 if __name__ == "__main__":
     main()
