e93b64447e7a80eb80ce7f1aba343577872b8fbc
Dante Mitchell <dantemit@umich.edu>
2023-12-12 11:55:23 -0500

    sql averages

--- Visualizations.py
+++ Visualizations.py
@@ -2,50 +2,36 @@ import json
 import sqlite3
 import matplotlib.pyplot as plt
 
-def get_wins_losses_json():
+def calculate_averages_sql():
     conn = sqlite3.connect('football_records_combined.db')
     c = conn.cursor()
 
-    c.execute('SELECT team_name, home_wins, home_losses, away_wins, away_losses, home_goal_diff, away_goal_diff  FROM football_records')
+    c.execute('''
+        SELECT 
+            AVG(home_wins) AS avg_home_wins,
+            AVG(home_losses) AS avg_home_losses,
+            AVG(away_wins) AS avg_away_wins,
+            AVG(away_losses) AS avg_away_losses,
+            AVG(home_goal_diff) AS avg_home_goal_diff,
+            AVG(away_goal_diff) AS avg_away_goal_diff
+        FROM football_records
+    ''')
 
-    data = c.fetchall()
-
+    averages = c.fetchone()
     conn.close()
 
-    json_data = json.dumps(data)
-
-    return json_data
-json_data = get_wins_losses_json()
-
-def calculate_averages(json_string):
-    
-    data = json.loads(json_string)
-    num_entries = len(data)
-    home_wins_total = sum(entry[1] for entry in data)
-    home_losses_total = sum(entry[2] for entry in data)
-    away_wins_total = sum(entry[3] for entry in data)
-    away_losses_total = sum(entry[4] for entry in data)
-    home_goal_diff_total = sum(entry[5] for entry in data)
-    away_goal_diff_total = sum(entry[6] for entry in data)
-
-    avg_home_wins = home_wins_total / num_entries
-    avg_home_losses = home_losses_total / num_entries
-    avg_away_wins = away_wins_total / num_entries
-    avg_away_losses = away_losses_total / num_entries
-    avg_home_goal_diff = home_goal_diff_total / num_entries
-    avg_away_goal_diff = away_goal_diff_total / num_entries
-
     return {
-        'Average Home Wins': avg_home_wins,
-        'Average Home Losses': avg_home_losses,
-        'Average Away Wins': avg_away_wins,
-        'Average Away Losses': avg_away_losses,
-        'Average Home Goal Differential': avg_home_goal_diff,
-        'Average Away Goal Differential': avg_away_goal_diff
+        'Average Home Wins': averages[0],
+        'Average Home Losses': averages[1],
+        'Average Away Wins': averages[2],
+        'Average Away Losses': averages[3],
+        'Average Home Goal Differential': averages[4],
+        'Average Away Goal Differential': averages[5]
     }
-averages = calculate_averages(json_data)
-categories = list(averages.keys())
-values = list(averages.values())
+
+averages_sql = calculate_averages_sql()
+categories = list(averages_sql.keys())
+values = list(averages_sql.values())
 
 plt.figure(figsize=(10, 6))
 
